d6616a399bb05e52e1ba3d97ea0a6974dde6aa27
Chris <chriscruz9000@proton.me>
2025-03-04 00:11:17 -0800

    Added mux

--- backend/cmd/track-my-app-api/main.go
+++ backend/cmd/track-my-app-api/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/rs/cors"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world"))
+	})
+	handler := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:5173"},
+		AllowedMethods: []string{"GET"},
+	}).Handler(mux)
+
+	log.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", handler)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module track-my-app-api
 
 go 1.23.6
+
+require github.com/rs/cors v1.11.1
