6507da431265b969b461d55c613b305500143fb1
Chris <chriscruz9000@proton.me>
2025-03-08 15:59:31 -0800

    Added release mongodb context to main

--- backend/cmd/track-my-app-api/main.go
+++ backend/cmd/track-my-app-api/main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"track-my-app-api/db"
 
 	"github.com/rs/cors"
 )
 
 func main() {
+	defer func() {
+		if err := db.Client.Disconnect(db.Ctx); err != nil {
+			log.Fatal("Error while disconnecting client: ", err)
+		}
+		db.Cancel() // Release the context
+		log.Println("Shutdown cleanup complete")
+	}()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello world"))
